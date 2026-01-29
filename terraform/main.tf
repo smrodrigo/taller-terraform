a4d17e49c15b1e87249039af1e0ff115e6698018
Salvador Merlos Rodrigo <smerlos@outlook.es>
2026-01-29 20:31:28 +0100

    Rename S3 bucket tag from 'sas_nodejs_bucket' to 'example_bucket'

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_s3_bucket" "tf_s3_bucket" {
   bucket = "tf-example-bucket02"
   tags = {
-    Name        = "sas_nodejs_bucket"
+    Name        = "example_bucket"
     Environment = "Dev"
   }
 }
